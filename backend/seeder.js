ec11513223cd05b7d0d393c05d788929d16406f8
AaronGanteng <152689131+AaronGanteng@users.noreply.github.com>
2026-06-23 23:44:55 +0700

    backend

--- backend/seeder.js
+++ backend/seeder.js
@@ -56,13 +56,134 @@ const sampleLocations = [
   {
     name: "Drop Box Sampah Elektronik - Kampus Sukolilo",
     address: "Keputih, Kec. Sukolilo, Surabaya (Dekat Halte Bus Kampus)",
-    accepted_items: ["Baterai", "Powerbank", "Smartphone Rusak", "Earphone"],
+    accepted_items: [
+      "Baterai",
+      "Powerbank",
+      "Smartphone Rusak/Lama",
+      "Earphone",
+    ],
     operational_hours: [
       { days: "Senin - Jumat", time: "07:00 - 21:00" },
       { days: "Sabtu - Minggu", time: "09:00 - 15:00" },
     ],
     latitude: -7.282345,
     longitude: 112.794123,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  // --------------------------------------------------
+  {
+    name: "Urban Republic - Galaxy Mall 3",
+    address:
+      "Galaxy Mall, Jl. Dr. Ir. H. Soekarno No.3, Mulyorejo, Kec. Mulyorejo, Surabaya, East Java",
+    accepted_items: [
+      "Kabel",
+      "Baterai",
+      "Smartphone Rusak/Lama",
+      "Powerbank",
+      "Adaptor Charger",
+    ],
+    operational_hours: [{ days: "Senin - Minggu", time: "10:00 - 22:00" }],
+    latitude: -7.2767005,
+    longitude: 112.7806097,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Urban Republic - Tunjungan Plaza 4",
+    address:
+      "Tunjungan plaza 4, Lantai 3, Kedungdoro, Kec. Tegalsari, Surabaya, East Java",
+    accepted_items: [
+      "Kabel",
+      "Baterai",
+      "Smartphone Rusak/Lama",
+      "Powerbank",
+      "Adaptor Charger",
+    ],
+    operational_hours: [{ days: "Senin - Minggu", time: "10:00 - 22:00" }],
+    latitude: -7.2614307,
+    longitude: 112.7385462,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Urban Republic - Pakuwon City Mall 3",
+    address:
+      "Pakuwon City Mall 3 Surabaya, Jl. Raya Laguna KJW Putih Tambak No.2 Lantai 2, Unit L2-51, Kejawaan Putih Tamba, Kec. Mulyorejo, Surabaya, East Java",
+    accepted_items: [
+      "Kabel",
+      "Baterai",
+      "Smartphone Rusak/Lama",
+      "Powerbank",
+      "Adaptor Charger",
+    ],
+    operational_hours: [{ days: "Senin - Minggu", time: "10:00 - 22:00" }],
+    latitude: -7.2766592,
+    longitude: 112.8061885,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Erafone - Megastore Galaxy Mall 3",
+    address:
+      "Galaxy Mall 3, Jl. Dr. Ir. H. Soekarno No.340 Lt.3 No. 339, Mulyorejo, Kec. Gubeng, Surabaya, East Java",
+    accepted_items: ["Kabel", "Baterai", "Smartphone Rusak/Lama"],
+    operational_hours: [{ days: "Senin - Minggu", time: "10:00 - 22:00" }],
+    latitude: -7.2766902,
+    longitude: 112.7803599,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Erafone - Plaza Marina Surabaya",
+    address:
+      "Plaza Marina, Lt. 3, Jl. Margorejo Indah Utara, Sidosermo, Wonocolo, Surabaya, East Java",
+    accepted_items: ["Adaptor Charger", "Baterai", "Smartphone Rusak/Lama"],
+    operational_hours: [{ days: "Senin - Minggu", time: "09:00 - 22:00" }],
+    latitude: -7.3156099,
+    longitude: 112.7486026,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Balai Kota Surabaya",
+    address:
+      "Jl. Taman Surya No.1, Ketabang, Kec. Genteng, Surabaya, East Java",
+    accepted_items: ["Baterai", "Gadget Kecil", "Lampu"],
+    operational_hours: [
+      { days: "Senin - Jumat", time: "08:00 - 16:00" },
+      { days: "Sabtu - Minggu", time: "Libur" },
+    ],
+    latitude: -7.2592031,
+    longitude: 112.7469525,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Dinas Lingkungan Hidup Provinsi Jawa Timur",
+    address:
+      "Jl. Wisata Menanggal No.38, Dukuh Menanggal, Kec. Gayungan, Surabaya, East Java",
+    accepted_items: ["Baterai", "Lampu", "Router/Modem"],
+    operational_hours: [
+      { days: "Senin - Jumat", time: "07:30 - 16:00" },
+      { days: "Sabtu - Minggu", time: "Libur" },
+    ],
+    latitude: -7.3461881,
+    longitude: 112.7342374,
+    image_url:
+      "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
+  },
+  {
+    name: "Bank Sampah Induk Surabaya",
+    address:
+      "Jl. Raya Menur No.31-A, Manyar Sabrangan, Kec. Mulyorejo, Surabaya, East Java",
+    accepted_items: ["Televisi", "Monitor", "Printer", "dll"],
+    operational_hours: [
+      { days: "Senin - Kamis dan Sabtu", time: "08:00 - 15:00" },
+      { days: "Jumat dan Minggu", time: "Libur" },
+    ],
+    latitude: -7.2782297,
+    longitude: 112.7629409,
     image_url:
       "https://res.cloudinary.com/ddcsuysdl/image/upload/v1780480370/sukolilo_uqtqys.jpg",
   },
